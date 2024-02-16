d5747d1c1905ec24b42120890b11cb1f046a6941
Michael Galambos <michael.galambos@agilita.ch>
2024-02-16 13:22:13 +0100

    implements multiple primary keys, bundles multiple api requests to same table into one, adds error handling to api requests, makes data from last expand be saved in each column individually

--- srv/service.js
+++ srv/service.js
@@ -51,7 +51,7 @@ class Service extends cds.ApplicationService {
     let data;
 
     if (isFromApi) {
-      const selectColumns = columns?.filter((column) => !column.ref);
+      const selectColumns = columns?.filter((column) => !column.expand);
       const where = req.query.SELECT.where;
       data = await this.apis
         .get(schemaEntity)
@@ -66,26 +66,25 @@ class Service extends cds.ApplicationService {
 
     if (!Array.isArray(data)) data = [data];
 
-    columns.forEach((column) => {
+    for (const column of columns) {
       if (column.expand) {
         column.previousEntityPath = currentEntityPath;
         column.hadRemote = isFromApi;
         column.path = [];
       }
-    });
+    }
 
-    await this._autoExpandColumns(data, columns, isFromApi);
-
+    await this._autoExpandColumns(data, columns, req);
     return data;
   }
 
-  async _autoExpandColumns(data, columns) {
+  async _autoExpandColumns(data, columns, req) {
     const definitions = cds.model.definitions;
     const expandObjects = [];
     const nextExpands = [];
 
-    columns.forEach((column, idx) => {
-      if (!column.expand) return;
+    for (const [i, column] of columns.entries()) {
+      if (!column.expand) continue;
 
       const schemaEntity = this._getSchemaEntity(
         definitions[column.previousEntityPath]
@@ -100,96 +99,136 @@ class Service extends cds.ApplicationService {
 
       if (expandObject.isRemote) column.hadRemote = true;
 
-      column.expand.forEach((nextColumn) => {
-        if (!nextColumn.expand) return;
+      for (const nextColumn of column.expand) {
+        if (!nextColumn.expand) continue;
         nextColumn.previousEntityPath = expandObject.previousEntityPath;
         nextColumn.path = [...column.path, column.ref[0]];
         nextColumn.hadRemote = column.hadRemote;
         nextExpands.push(nextColumn);
-      });
+      }
 
-      if (!column.hadRemote) return;
-      expandObject.index = idx;
+      if (!column.hadRemote) continue;
+      expandObject.index = i;
       expandObject.path = column.path;
       expandObjects.push(expandObject);
-    });
+    }
 
-    expandObjects.forEach((expandObject) => {
-      if (!expandObject.isRemote) return;
-      this._addId(columns, expandObject.associationKey);
-    });
+    for (const expandObject of expandObjects) {
+      if (!expandObject.isRemote) continue;
+      this._addId(columns, expandObject.associationKeys);
+    }
+
+    for (const [i, expandObject] of expandObjects.entries()) {
+      const expandColumns = columns[expandObject.index].expand;
+      this._addId(expandColumns, expandObject.keys);
+      let j = 0;
+      const expandKeys = data
+        .reduce((expandColumn, items) => {
+          if (!Array.isArray(items)) items = [items];
+          items = this._findItem(items, expandObject.path);
+
+          for (const item of items) {
+            expandColumn[j] ??= [];
+
+            expandObject.associationKeys.forEach((key) => {
+              if (item && item[key]) {
+                expandColumn[j].push(item[key]);
+              }
+            });
+            j++;
+          }
+          return expandColumn;
+        }, [])
+        .getUnique();
+
+      if (expandKeys.length <= 0) {
+        expandObjects.splice(i, 1);
+        continue;
+      }
+
+      expandObject.expandKeys = expandKeys;
+      expandObject.expandColumns = expandColumns.filter(
+        (column) => !column.expand
+      );
+    }
+
+    // array vs map i dont know whats faster. id assume map
+    const mExpandObjects = new Map();
+
+    for (const expandObject of expandObjects) {
+      const entity = expandObject.entity;
+      let expandColumns = expandObject.expandColumns;
+      let expandKeys = expandObject.expandKeys;
+      if (mExpandObjects.has(expandObject.entity)) {
+        const existingObject = mExpandObjects.get(expandObject.entity);
+        expandColumns = [
+          ...new Set([...expandColumns, ...existingObject.expandColumns]),
+        ].getUnique();
+        expandKeys = [...expandKeys, ...existingObject.expandKeys].getUnique();
+      }
+
+      mExpandObjects.set(entity, {
+        entity: entity,
+        expandColumns: expandColumns,
+        expandKeys: expandKeys,
+        isRemote: expandObject.isRemote,
+        service: expandObject.service || null,
+        expandKeyNames: expandObject.keys,
+      });
+    }
 
     const expands = [];
+    const expandKeys = [];
 
-    for (let i = 0; i < expandObjects.length; i++) {
-      const expandObject = expandObjects[i];
-      const expandColumns = columns[expandObject.index].expand;
-      let expand;
+    for (const [key, expandObject] of mExpandObjects) {
+      let service;
+      let where = {};
+      let lastWhere = where;
+      let columns = expandObject.expandColumns;
 
-      if (expandObject.isRemote) {
-        this._addId(expandColumns, expandObject.key);
-
-        const expandIDs = [
-          ...new Set(
-            data.reduce((expandColumn, items) => {
-              if (!Array.isArray(items)) items = [items];
-              items = this._findItem(items, expandObject.path);
-
-              expandObject.associationKey.forEach((key, i) => {
-                expandColumn[i] ??= [];
-
-                items.forEach((item) => {
-                  if (item && item[key]) {
-                    expandColumn[i].push(item[key]);
-                  }
-                });
-              });
-
-              return expandColumn;
-            }, [])
-          ),
-        ];
-
-        if (expandIDs.length <= 0) {
-          expands.push(null);
-          continue;
+      for (const [i, expandKey] of expandObject.expandKeys.entries()) {
+        for (const [
+          j,
+          expandKeyName,
+        ] of expandObject.expandKeyNames.entries()) {
+          lastWhere[expandKeyName] = expandKey[j];
         }
-
-        let where = {};
-        let previous = where;
-        // dont add douplicates to where
-        for (let j = 0; j < expandIDs[0].length; j++) {
-          expandObject.key.forEach((key, idx) => {
-            if (previous[key]) {
-              previous.or = {};
-              previous = previous.or;
-            }
-            previous[key] = expandIDs[idx][j];
-          });
+        if (expandObject.expandKeys[i + 1]) {
+          lastWhere.or = {};
+          lastWhere = lastWhere.or;
         }
-
-        const selectColumns = expandColumns.filter((column) => !column.ref);
-
-        expand = this.apis
-          .get(expandObject.service)
-          .run(SELECT(selectColumns).from(expandObject.entity).where(where));
-      } else {
-        const selectColumns = expandColumns.filter((column) => !column.ref);
-        expand = this.run(SELECT(selectColumns).from(expandObject.entity)); // where keys arent null
       }
+      if (expandObject.isRemote) {
+        service = this.apis.get(expandObject.service);
+      } else {
+        service = this;
+      }
+      if (columns.indexOf("*") !== -1) columns = ["*"];
+      const expand = service.run(
+        SELECT(columns).from(expandObject.entity).where(where)
+      );
+      expandKeys.push(key);
       expands.push(expand);
     }
 
-    // make requests from same table to one prmise
-    await Promise.all(expands).then((res) => {
-      for (let i = 0; i < res.length; i++) {
-        if (!res[i]) continue;
-        this._expand(data, res[i], expandObjects[i]);
-      }
-    });
+    let res;
+    try {
+      res = await Promise.all(expands);
+    } catch (e) {
+      data = { error: e };
+      return req.reject();
+    }
+
+    for (const [i, key] of expandKeys.entries()) {
+      mExpandObjects.set(key, res[i]);
+    }
+
+    for (const expandObject of expandObjects) {
+      this._expand(data, mExpandObjects.get(expandObject.entity), expandObject);
+    }
 
     if (nextExpands.length !== 0) {
-      await this._autoExpandColumns(data, nextExpands);
+      await this._autoExpandColumns(data, nextExpands, req);
     }
 
     return data;
@@ -210,17 +249,17 @@ class Service extends cds.ApplicationService {
 
     const service = entitySchema.projection?.from?.ref[0]?.split(".")[0];
 
-    let key;
-    let associationKey;
+    let keys;
+    let associationKeys;
 
     if (association.keys) {
-      const associationKeys = association.keys;
-      key = associationKeys.reduce((keys, key) => {
+      const associationKey = association.keys;
+      keys = associationKey.reduce((keys, key) => {
         keys.push(key.ref[0]);
         return keys;
       }, []);
 
-      associationKey = associationKeys.reduce((keys, key) => {
+      associationKeys = associationKey.reduce((keys, key) => {
         keys.push(key.$generatedFieldName);
         return keys;
       }, []);
@@ -229,15 +268,15 @@ class Service extends cds.ApplicationService {
       if (association.on[0].ref[0] === "$self") {
         idx = 2;
       }
-      const associationKeys =
+      const associationKey =
         entitySchema.associations[association.on[idx].ref[1]].keys;
 
-      associationKey = associationKeys.reduce((keys, key) => {
+      associationKeys = associationKey.reduce((keys, key) => {
         keys.push(key.ref[0]);
         return keys;
       }, []);
 
-      key = associationKeys.reduce((keys, key) => {
+      keys = associationKey.reduce((keys, key) => {
         keys.push(key.$generatedFieldName);
         return keys;
       }, []);
@@ -246,8 +285,8 @@ class Service extends cds.ApplicationService {
     const result = {
       isRemote: isFromRemote,
       associationName: associationName,
-      associationKey: associationKey,
-      key: key,
+      associationKeys: associationKeys,
+      keys: keys,
       previousEntityPath: expandEntityName,
       entity: entitySchema,
     };
@@ -256,13 +295,13 @@ class Service extends cds.ApplicationService {
       result.entity = entity;
       result.service = service;
     }
-    
+
     return result;
   }
 
   _addId(columns, keys) {
     const allSelected = columns.indexOf("*") !== -1;
-    keys.forEach((key) => {
+    for (const key of keys) {
       const idSelected = columns.find(
         (column) => column.ref && column.ref.find((ref) => ref == key)
       );
@@ -270,7 +309,7 @@ class Service extends cds.ApplicationService {
       if (!allSelected && !idSelected) {
         columns.push({ ref: [key] });
       }
-    });
+    }
   }
 
   _getSchemaEntity(entity) {
@@ -281,50 +320,72 @@ class Service extends cds.ApplicationService {
     return entity;
   }
 
-  _expand(data, expands, expandObject) {
+  _expand(data, res, expandObject) {
+    const columns = expandObject.expandColumns;
+    let expands;
+    if (columns.indexOf("*") === -1) {
+      let keys = new Set();
+
+      for (const column of columns) keys.add(column?.ref[0]);
+
+      expands = res.reduce((expands, expand) => {
+        const expandKeys = Object.keys(expand);
+        let object = {};
+
+        for (const key of expandKeys) {
+          if (keys.has(key)) {
+            object[key] = expand[key];
+          }
+        }
+        expands.push(object);
+        return expands;
+      }, []);
+    } else expands = res;
+
     const mExpands = new Map();
 
-    expands.forEach((expand) => {
+    for (const expand of expands) {
       let keys = [];
-      expandObject.key.forEach((key) => {
-        keys.push(expand[key]);
-      });
+
+      for (const key of expandObject.keys) keys.push(expand[key]);
+
       keys = keys.join("::");
       if (mExpands.has(keys)) {
         mExpands.set(keys, [...mExpands.get(keys), expand]);
-        return;
+        continue;
       }
 
       mExpands.set(keys, [expand]);
-    });
+    }
 
-    mExpands.forEach((expand, key) => {
-      if (expand.length === 1) mExpands.set(key, expand[0]);
-    });
-
-    data.forEach((items) => {
+    for (let items of data) {
       if (!Array.isArray(items)) items = [items];
       items = this._findItem(items, expandObject.path);
 
-      if (!items) return;
-      items.forEach((item) => {
-        if (!item) return;
+      if (!items) continue;
+      for (const item of items) {
+        if (!item) continue;
 
         let keys = [];
-        expandObject.associationKey.forEach((key) => {
+
+        for (const key of expandObject.associationKeys) {
           keys.push(item[key]);
-        });
+        }
         keys = keys.join("::");
 
         item[expandObject.associationName] = mExpands.get(keys);
-      });
-    });
+      }
+    }
   }
 
   _findItem(items, path) {
     if (path.length === 0) return items;
-    items.forEach((item, i) => {
-      if (!item[path[0]]) return;
+
+    for (const [i, item] of items.entries()) {
+      if (!item[path[0]]) {
+        items.splice(i, 1);
+        continue;
+      }
 
       if (Array.isArray(item[path[0]])) {
         item[path[0]].forEach((item) => {
@@ -335,9 +396,15 @@ class Service extends cds.ApplicationService {
       } else {
         items[i] = item[path[0]];
       }
-    });
+    }
     items = this._findItem(items, path.slice(1));
     return items;
   }
 }
 module.exports = Service;
+
+Array.prototype.getUnique = function () {
+  return this.filter((a) => !(0 - (this[a] = ++this[a] | 0)));
+};
+
+// IDs used only for expand arend deleted afterwards
